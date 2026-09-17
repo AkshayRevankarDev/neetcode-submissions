d30a0267ec977e4fd3d0938297f7007a644ba9d5
AkshayRevankar <arevanka@buffalo.edu>
2026-09-17 18:03:09 -0400

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        window = set()
+
+        for n in nums:
+            if n in window:
+                return True
+            window.add(n)
+
+        return False
---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count={}
+
+        for n in nums:
+            count[n]= 1 + count.get(n,0)
+
+        return sorted(count,key=lambda x:count[x])[-k:]
+
+        
